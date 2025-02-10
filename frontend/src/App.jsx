ad05396d5399e1c33870ee0fb454f4e9f6a8f7ac
adarshaady17 <adarshsahu6236@gmail.com>
2025-02-10 18:36:52 +0530

    Add new files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,31 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import "./App.css";
+import Navbar from "./components/shared/Navbar";
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home from './components/Home'
+
+
+const appRouter=createBrowserRouter([{
+  path:"/",
+  element:<Home/>
+},
+{
+  path:"/login",
+  element:<Login/>
+},
+{
+  path:"/signup",
+  element:<Signup/>
+},
+])
 
 function App() {
   return (
     <>
-      <h1>Hi i am adrsh</h1>
+      <RouterProvider router={appRouter} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { LogOut, User2 } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+// import { useDispatch, useSelector } from 'react-redux'
+// import axios from 'axios'
+// import { USER_API_END_POINT } from '@/utils/constant'
+// import { setUser } from '@/redux/authSlice'
+// import { toast } from 'sonner'
+
+const Navbar = () => {
+  // const { user } = useSelector(store => store.auth);
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
+
+  // const logoutHandler = async () => {
+  //     try {
+  //         const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+  //         if (res.data.success) {
+  //             dispatch(setUser(null));
+  //             navigate("/");
+  //             toast.success(res.data.message);
+  //         }
+  //     } catch (error) {
+  //         console.log(error);
+  //         toast.error(error.response.data.message);
+  //     }
+  // }
+  const user=false;
+  return (
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-10 max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Career<span className="text-[#F83002]">Friendly</span>
+          </h1>
+        </div>
+        <div className="flex items-center gap-12">
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
+
+          {
+            !user?(
+                <div className="flex items-center gap-2">
+                <Link to="/login"><Button variant="outline">Login</Button></Link>
+                <Link to="/signup"><Button className="bg-[#6A38C2] hover:bg-[#1c0e35]">Signup</Button></Link>    
+                </div>
+            ):(
+                <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage
+                  scr="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+              <div>
+                <div className="flex gap-2 space-y-2">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      scr="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium">Adarsh MernStack</h4>
+                    <p className="text-sm text-muted-foreground">
+                      Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col my-2 text-gray-600">
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <User2/>
+                    <Button variant="link">View Profile</Button>
+                  </div>
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                  <LogOut/>
+                    <Button variant="link">Logout</Button>
+                  </div>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
