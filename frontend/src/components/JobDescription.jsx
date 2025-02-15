e9aa16242445a1431b8ea82fa314b7dd9f6bef74
adarshaady17 <adarshsahu6236@gmail.com>
2025-02-15 23:29:28 +0530

    Add modified file

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Badge } from './ui/badge'
 import { Button } from './ui/button'
-//import { useParams } from 'react-router-dom';
-//import axios from 'axios';
-//import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant';
-// import { setSingleJob } from '@/redux/jobSlice';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant';
+import { setSingleJob } from '@/redux/jobSlice';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { toast } from 'sonner';
 
@@ -14,8 +14,9 @@ const JobDescription = () => {
     // const isIntiallyApplied = singleJob?.applications?.some(application => application.applicant === user?._id) || false;
     // const [isApplied, setIsApplied] = useState(isIntiallyApplied);
 
-    // const params = useParams();
-    // const jobId = params.id;
+    const isApplied=true;
+    const params = useParams();
+    const jobId = params.id;
     // const dispatch = useDispatch();
 
     // const applyJobHandler = async () => {
@@ -35,21 +36,20 @@ const JobDescription = () => {
     //     }
     // }
 
-    // useEffect(()=>{
-    //     const fetchSingleJob = async () => {
-    //         try {
-    //             const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`,{withCredentials:true});
-    //             if(res.data.success){
-    //                 dispatch(setSingleJob(res.data.job));
-    //                 setIsApplied(res.data.job.applications.some(application=>application.applicant === user?._id)) // Ensure the state is in sync with fetched data
-    //             }
-    //         } catch (error) {
-    //             console.log(error);
-    //         }
-    //     }
-    //     fetchSingleJob(); 
-    // },[jobId,dispatch, user?._id]);
-    const isApplied=true;
+    useEffect(()=>{
+        const fetchSingleJob = async () => {
+            try {
+                const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`,{withCredentials:true});
+                if(res.data.success){
+                    dispatch(setSingleJob(res.data.job));
+                    setIsApplied(res.data.job.applications.some(application=>application.applicant === user?._id)) // Ensure the state is in sync with fetched data
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchSingleJob(); 
+    },[jobId,dispatch, user?._id]);
     return (
         <div className='max-w-7xl mx-10 my-10'>
             <div className='flex items-center justify-between'>
--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -2,13 +2,13 @@ import React  from 'react'
 import Navbar from './shared/Navbar'
 import FilterCard from './FilterCard'
 import Job from './Job';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 // import { motion } from 'framer-motion';
 
 const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const Jobs = () => {
-    // const { allJobs, searchedQuery } = useSelector(store => store.job);
+    const { allJobs, searchedQuery } = useSelector(store => store.job);
     // const [filterJobs, setFilterJobs] = useState(allJobs);
 
     // useEffect(() => {
@@ -33,13 +33,13 @@ const Jobs = () => {
                         <FilterCard />
                     </div>
                     {
-                        jobsArray.length<=0 ? <span>Job not found</span> : (
+                        allJobs.length<=0 ? <span>Job not found</span> : (
                             <div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
                                 <div className='grid grid-cols-3 gap-4'>
                                     {
-                                        jobsArray.map((item,index) =>(
-                                            <div>
-                                                <Job />
+                                        allJobs.map((job) =>(
+                                            <div key={job?._id}>
+                                                <Job job={job}/>
                                             </div>
                                         ))
                                     }
