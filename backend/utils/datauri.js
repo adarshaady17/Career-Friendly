1015b49e9eae9a5b31f6901f02b024db0184d09a
adarshaady17 <adarshsahu6236@gmail.com>
2025-03-01 19:32:15 +0530

    Add modified file

--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -4,9 +4,7 @@ import path from "path";
 
 const getDataUri = (file) => {
     const parser = new DataUriParser();
-    //console.log("b4");
     const extName = path.extname(file.originalname).toString();
-    //console.log("after");
     return parser.format(extName, file.buffer);
 }
 
